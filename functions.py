bdefb78a3accc1af9569dea131ee82be6a440d57
Abdelrahman Ali <93502098+abdelrahman-ali123@users.noreply.github.com>
2023-01-02 02:10:35 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,46 +2,48 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+class Image:
+    def _init_(self, filename, filepath):
+        self.filename = filename
+        self.filepath = filepath
+     
+    def getresize(self):
+        original_img = cv2.imread(self.filepath, 0)
+        img =  cv2.resize(original_img, (1440, 1080))   
+        return img
 
-#upload images and resize them
-img1_path = cv2.imread("static\IMG\img2.jpg", 0)
-first_img= cv2.resize(img1_path, (1440, 1080))
+    def getfft( img ):
+        img_fft = np.fft.fftshift(np.fft.fft2(img))
+        return img_fft
 
-img2_path = cv2.imread("static\IMG\img7.jpg", 0)
-second_img= cv2.resize(img2_path, (1440, 1080))
+    def getmag(self, img_fft):
+        # img_mag = np.sqrt(np.real(img_fft) * 2 + np.imag(img_fft) * 2)
+        img_mag = np.abs(img_fft)
+        magpath = (f"static/IMG/{self.filename}_mag.jpg")
+        plt.imsave(magpath , np.log(img_mag+1e-10) , cmap='gray')
+        return magpath, img_mag
 
-#appling fft 
-first_img_fft = np.fft.fftshift(np.fft.fft2(first_img))
-second_img_fft = np.fft.fftshift(np.fft.fft2(second_img))
-
-#git amplitude and phase
-
-# first_img_amplitude = np.sqrt(np.real(first_img_fft) ** 2 + np.imag(first_img_fft) ** 2)
-# first_img_phase = np.arctan2(np.imag(first_img_fft), np.real(first_img_fft))
-# second_img_amplitude = np.sqrt(np.real(second_img_fft) ** 2 + np.imag(second_img_fft) ** 2)
-# second_img_phase = np.arctan2(np.imag(second_img_fft), np.real(second_img_fft))
-
-first_img_amplitude = np.abs(first_img_fft)
-first_img_phase = np.angle(first_img_fft)
-second_img_amplitude = np.abs(second_img_fft)
-second_img_phase = np.angle(second_img_fft)
-
-#merge between images
-first_second_imgs_combine = np.multiply(first_img_amplitude, np.exp(1j * second_img_phase))
-first_second_imgs = np.real(np.fft.ifft2(first_second_imgs_combine))  
-
-second_first_imgs_combine = np.multiply(second_img_amplitude, np.exp(1j * first_img_phase))
-second_first_imgs = np.real(np.fft.ifft2(np.fft.ifftshift(second_first_imgs_combine)))
-
-#removing specific range of frequencies
-new_first_img_amplitude=first_img_amplitude.copy() #take a copy of the magnitude 
-new_first_img_phase=first_img_phase.copy()#take a copy of the phase
-for i in range(first_img_amplitude.shape[1]):
-    for j in range(first_img_amplitude.shape[0]):
-        if (i >=500 and i<=600 )and (j>=700 and j <=800 ):
-            new_first_img_amplitude[i][j]=1
-            new_first_img_phase[i][j]=0
-
-#reconstruct signal
-imgs_comb = np.multiply((new_first_img_amplitude), np.exp(1j * new_first_img_phase))
-first_second_imgs = np.real(np.fft.ifft2(np.fft.ifftshift(imgs_comb)))
+    def getphase(self, img_fft):
+        # img_phase = np.arctan2(np.imag(img_fft), np.real(img_fft))
+        img_phase = np.angle(img_fft)
+        phasepath = (f"static/IMG/{self.filename}_phase.jpg")
+        plt.imsave(phasepath, img_phase, cmap='gray')
+        return phasepath, img_phase
+    
+    def freqrange(img_mag , img_phase ,y1,y2, x1,x2):
+        new_img_mag = img_mag.copy() #take a copy of the magnitude
+        new_img_phase = img_phase.copy() #take a copy of the phase
+        for i in range(img_mag.shape[0]):
+            for j in range(img_phase.shape[1]):
+                if (i <=y1 or i>=y2 ) or (j<=x1 or j>=x2):
+                    new_img_mag[i][j]=1
+                    new_img_phase[i][j]=0
+        return new_img_mag , new_img_phase
+    
+    def imgcombine(self , new_img_mag , new_img_phase):
+        merging = np.multiply(new_img_mag, np.exp(1j * new_img_phase))
+        img_combine = np.real(np.fft.ifft2(np.fft.ifftshift(merging)))
+        combinepath = (f"static/IMG/{self.filename}_combine.jpg")
+        plt.imsave(combinepath, img_combine, cmap='gray')
+        return combinepath , img_combine
+    
